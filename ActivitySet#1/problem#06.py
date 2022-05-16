6a1232461cbe5614295e9acd598402cde5575085
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-05-16 03:42:26 +0000

    .

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,18 @@
-# Loops & Iterators
-
 largest = None
 smallest = None
 
 while True:
     num = input("Enter a number? ")
-
     if num == "done":
         break
-
-    # ...
-
-    print(num)
+    try:
+        n=int(num)
+        if largest is None or n > largest:
+            largest = n
+        if smallest is None or n < smallest:
+            smallest = n
+    except:
+            print('Invalid input')
 
 print("Maximum", largest)
+print("Minimum ", smallest)
