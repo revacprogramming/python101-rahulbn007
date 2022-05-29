428662057065f1f669ace6394cfedbded7393067
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-05-29 16:55:12 +0000

    .

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -2,18 +2,18 @@
 name = input("Enter file:")
 if len(name) < 1:
      name = "mbox-short.txt"
-handle = open(name)
+h = open(name)
 d={}
 t=[]
-for line in handle:
-    if line.startswith('From:'): continue
-    if not line.startswith('From'): continue
-    index=line.find(':')
-    st=line[index-2:index]
-    if st not in d:
-        d[st]=1
+for l in h:
+    if l.startswith('From:'): continue
+    if not l.startswith('From'): continue
+    index=l.find(':')
+    s=l[index-2:index]
+    if s not in d:
+        d[s]=1
     else:
-        d[st]+=1
+        d[s]+=1
 
 t = list(d.items())
 t.sort()
