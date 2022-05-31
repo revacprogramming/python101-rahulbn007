daac63c031efd4776e502f6514a11d1f9021c053
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-05-31 01:41:04 +0000

    .

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,11 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+# Regular Expressions
+# https://www.py4e.com/lessons/regex
+import re
+hand=input('Enter the file name:')
+handle=open(hand)
+total=0
+for line in handle:
+     line=line.rstrip()
+     ss=re.find
