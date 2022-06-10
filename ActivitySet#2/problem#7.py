ded91adf5a6d3fc36d5195e94a0682046f674991
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-06-10 18:17:50 +0000

    .

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -2,8 +2,5 @@ class Menu:
   my_menu=[]
   def __init__(self,name='',price=0):
     self.my_menu.append((name,price))
-  def __add__(self,a):
-    return Menu(a[0],a[1])
-  def __str__(self):
-    sd=self.my_menu
+  def __add__(self,a
   
