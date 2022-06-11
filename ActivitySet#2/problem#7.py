e5e3d87bcc0289c5b0d14df1a9a4648f211503ca
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-06-11 16:40:16 +0000

    .

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -2,5 +2,11 @@ class Menu:
   my_menu=[]
   def __init__(self,name='',price=0):
     self.my_menu.append((name,price))
-  def __add__(self,a
+  def __add__(self,a):
+    self.my_menu.append((a[0],a[1]))
+    return self
+  def __str__(self):
+    sd=self.my_menu[1:]
+    return ("\n".join([str(a+' '+str(b)) for a,b in sd]))
+
   
