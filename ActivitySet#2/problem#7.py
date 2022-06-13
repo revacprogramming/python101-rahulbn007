9f65c12a7bb2e8a534e69e05a2019357a1028746
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-06-13 16:29:22 +0000

    .

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -8,5 +8,9 @@ class Menu:
   def __str__(self):
     sd=self.my_menu[1:]
     return ("\n".join([str(a+' '+str(b)) for a,b in sd]))
+m = Menu()
 
-  
+m = m + ("idly", 10) + ("vada", 20) + ('dosa',50)
+
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
+print(m)  # should print the menu properly
