9a667424ff10c566b2aaa84ac2d1afe08fd5cb50
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-06-27 03:10:30 +0000

    .

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,20 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+import socket
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+url=input('enter the url- ')
+counts=int(input('enter count: '))
+position=int(input('enter position: '))
+for i in range(counts):
+    count=0
+    html=urlopen(url).read()
+    soup=BeautifulSoup(html,'html.parser')
+    # print(count)
+    tags = soup('a')
+    for tag in tags:
+        if count<position:
+            # print(tag.get('href'))
+            count+=1
+            url=tag.get('href')
+    print('Retrieving:',url)
--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,11 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect(('data.pr4e.org', 80))
+call='GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+socket.send(call)
+while True:
+    data=socket.recv(1000)
+    if len(data)<1:
+        break
+    print(data.decode())
