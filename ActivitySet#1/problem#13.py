f3d5650100162e0c3ec87033b2c37de6eb643939
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-07-04 17:31:16 +0000

    .

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -6,7 +6,6 @@ from urllib.request import urlopen
 url=input('enter the url- ')
 counts=int(input('enter count: '))
 position=int(input('enter position: '))
-
 for i in range(counts):
     count=0
     html=urlopen(url).read()
