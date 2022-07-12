cc7fff408e568eb99b850b76df0c42dadac27b2a
rahulbn007 <96820417+rahulbn007@users.noreply.github.com>
2022-07-12 11:05:47 +0000

    .

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,3 +1,17 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
-class object
+import urllib.request
+import urllib.parse
+import urllib.error
+import bs4
+from bs4 import BeautifulSoup
+
+url =' http://py4e-data.dr-chuck.net/comments_1547251.html'
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+sum = 0
+tags = soup('span')
+for tag in tags:
+    sum = sum+int(tag.contents[0])
+print(sum)
